0a917104a3c5414e82085064f1de0f6a71837f9b
Dmitri9149 <dmitri.as.name@gmail.com>
2024-10-10 15:19:01 +0300

    modify the original web page

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,17 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>CI/CD with React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          The task is to integrate 'robot framework' tests in pipeline  ...
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        And at the moment I do not know how, but will know soon, I guess ...
       </p>
     </>
   )
